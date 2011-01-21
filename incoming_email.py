011b5aad130b7a2666a7652b8ff4ad90f8e5cde2
Kevin Morey <kevin.morey@gmail.com>
2011-01-21 13:27:29 +0800

    Improve comment parsing and error handling
    * Use a large regex to parse the comment, rather than splitting by lines.
      This should be more flexible since email can line break in odd places.
    
    * Add split by signature
    
    * Reply to the email if an error occurs and their comment can not be added.

--- incoming_email.py
+++ incoming_email.py
@@ -8,11 +8,13 @@ import urllib
 from google.appengine.ext import db
 from models import Tokens
 from models import Comments
+from google.appengine.api import mail
 
 class IncomingEmailHandler(InboundMailHandler):
-
-    def receive(self, message):
     
+    noreply="PT-Mail <noreply@pt-mail.appspotmail.com>"
+        
+    def receive(self, message):
         p = re.compile('([-a-z0-9_.+]+@([-a-z0-9]+\.)+[a-z]{2,6})',re.IGNORECASE)
         m = p.search(message.sender)
         if m is not None:
@@ -23,51 +25,64 @@ class IncomingEmailHandler(InboundMailHandler):
 
         sender = sender.lower()        
         logging.info("Received a message from: " + sender)
-        tokens = db.Query(Tokens).filter('pt_email =', sender).get()
-        mytoken = tokens.pt_token
         
         plaintext_bodies = message.bodies('text/plain')
-    
         message_body = ''
         for content_type, body in plaintext_bodies:
             message_body += body.decode()
+            
+        tokens = db.Query(Tokens).filter('pt_email =', sender).get()
+        
+        if tokens is None:
+            self.logAndReply(sender, "Could not find your PT token. Have you signed up yet? Your comment will not be added.\n\nOriginal reply:\n%s" % (message_body))
+            return
+            
+        mytoken = tokens.pt_token
 
         storyId = self.getStoryId(message_body)  
         
         if storyId == False:
-            logging.info("Could not find the story Id. Your comment will not be added")
-        	return          
+            self.logAndReply(sender, "Could not find the story Id. Your comment will not be added.\n\nOriginal reply:\n%s" % (message_body))
+            return          
 
         projectId = self.getProjectIdFromStoryId(mytoken, storyId)
     
         if projectId != False:
             logging.info("Using ProjectId: " + projectId + " StoryId: " + storyId)
         else:
-            logging.info("Could not find the project for this story. Your comment will not be added")
+            self.logAndReply(sender, "Could not find the project for this story. Your comment will not be added.\n\nOriginal reply:\n%s" % (message_body))
+            return
 
         if tokens.signature is None:
             signature = ''
         else:
             signature = tokens.signature
-			
+            
         comment = self.getComment(message_body, signature)
+        if comment is None:
+            self.logAndReply(sender, "Could not figure out what your comment was.\n\nOriginal reply:\n%s" % (message_body))
+            return
+            
         logging.info(comment)
             
         self.postToPT(mytoken, projectId, storyId, comment)
         
-        comment = Comments(token=tokens, projectId=projectId, storyId=storyId, comment = db.Text(comment))
-        
+        comment = Comments(token=tokens, projectId=projectId, storyId=storyId, comment = db.Text(comment))        
         db.put(comment)
-        
+
         return
             
-            
+    def logAndReply(self, sender, error):
+        logging.error(error)
+        mail.send_mail(sender=self.noreply, to=sender, subject="PT-Mail Error", body=error)
+        mail.send_mail(sender=self.noreply, to=('kevin.morey@gmail.com', 'chrisvaughn01@gmail.com'), subject="PT-Mail Error", body=error)
+        
     def getStoryId(self, body):
         m = re.search('http[s]?://www.pivotaltracker.com/story/show/(\d+)', body)
         if m is not None:
-	        return m.group(1)
-	    else:
-	    	return False
+            return m.group(1)
+        else:
+            return False
         
     
     def getProjectIdFromStoryId(self, token, storyId):
@@ -94,30 +109,42 @@ class IncomingEmailHandler(InboundMailHandler):
     
     def getComment(self, body, signature):
         
-        lines = body.split('\n')
-        comment = ''
+        if signature is None or signature == "":
+            re_str = '(.*?)(?:(?:\r|\n)>|From: Pivotal Tracker|(?:\r|\n)On .*? wrote:|Begin forwarded message:)'
+        else:
+            re_str = '(.*?)(?:(?:\r|\n)%s(?:\r|\n)|(?:\r|\n)>|From: Pivotal Tracker|(?:\r|\n)On .*? wrote:|Begin forwarded message:)' % (signature)
+
+        comment = re.search(re_str, body, re.I | re.S)
         
-        for line in lines:
+        if comment is None:
+            return None
             
-            m = re.search('^>', line)
-            m2 = re.search('From: Pivotal Tracker', line)
-            
-            if m is None and m2 is None:
-                line += '\n'
-                comment += line
-            else:
-                break
-            
-        #if user has a signature stored then remove it
-        comment = re.sub(signature, '', comment)
+        comment = comment.group(1)
+
+        ### OLD CODE
+        # lines = body.split('\n')
+        #         comment = ''
+        #         
+        #         for line in lines:
+        #             
+        #             m = re.search('^>|From: Pivotal Tracker|^On .*?\n?.*? wrote:$', line)
+        #             #m2 = re.search('From: Pivotal Tracker', line)
+        #             
+        #             if m is None: # and m2 is None:
+        #                 line += '\n'
+        #                 comment += line
+        #             else:
+        #                 break
+        #
+
+        #if user has a signature stored then remove it (if it wasn't matched)
+        #comment = re.sub(signature, '', comment)
 
         comment = re.sub('Begin forwarded message:', '', comment)
         comment = re.sub('On.*wrote:\n', '', comment)
         comment = re.sub('_+\n', '', comment)
         comment = re.sub('\n\n+', '\n\n', comment)
         
-
-                
         return comment
     
     def postToPT(self, token, projectId, storyId, comment):
