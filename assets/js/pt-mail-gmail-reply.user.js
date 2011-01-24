af709fa461c88f4394a88511c86ce38b03ff727d
Kevin Morey <kevin.morey@gmail.com>
2011-01-23 22:59:57 -0600

    Add GreaseMonkey script to fix PT reply "to" field

---
+++ assets/js/pt-mail-gmail-reply.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name          PT-Mail Gmail Reply
+// @namespace     http://mail.google.com/
+// @version 	  1.0
+// @author        kmorey
+// @description   When replying to a Pivotal Tracker email, this will replace the no-reply address in the 'to' field with pt@pt-mail.appspotmail.com automatically.
+// @include       http://mail.google.com/*
+// @include       https://mail.google.com/*
+// ==/UserScript==
+
+window.addEventListener('load', function () {
+	// Note: only works in FF+GreaseMonkey because WebKit doesn't support DOMAttrModified yet
+	document.addEventListener('DOMAttrModified', start, true);
+}, true);
+
+function start(e){
+	
+	setTimeout(function() {
+		if (e.target.name === 'to') {
+			var to = e.target;
+			var regex = /tracker-noreply@pivotaltracker\.com/;
+
+			if (to.value.match(regex)) {
+				to.value = 'PT-Mail <pt@pt-mail.appspotmail.com>';
+			}
+		}
+	}, 10);
+}
